1087adb5dcdbfbc7d5db2c88638721ae63a79849
emikoarchv <heruprtm257@gmail.com>
2026-05-24 09:16:52 +0700

    Update main.cpp

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -5,7 +5,7 @@
 #include <GLES2/gl2.h>
 #include <vulkan/vulkan.h>
 #include <dlfcn.h>
-#include "zygisk.h"
+#include "zygisk.hpp"
 
 // =============================================================================
 // HOOKING OPENGL ES (Adreno 830)
@@ -14,15 +14,9 @@ typedef const GLubyte* (*PFNGLGETSTRINGPROC)(GLenum name);
 PFNGLGETSTRINGPROC orig_glGetString = nullptr;
 
 const GLubyte* hooked_glGetString(GLenum name) {
-    if (name == GL_RENDERER) {
-        return (const GLubyte*)"Adreno (TM) 830";
-    }
-    if (name == GL_VENDOR) {
-        return (const GLubyte*)"Qualcomm";
-    }
-    if (orig_glGetString) {
-        return orig_glGetString(name);
-    }
+    if (name == GL_RENDERER) return (const GLubyte*)"Adreno (TM) 830";
+    if (name == GL_VENDOR) return (const GLubyte*)"Qualcomm";
+    if (orig_glGetString) return orig_glGetString(name);
     return (const GLubyte*)"";
 }
 
@@ -33,9 +27,7 @@ typedef void (*PFN_vkGetPhysicalDeviceProperties)(VkPhysicalDevice physicalDevic
 PFN_vkGetPhysicalDeviceProperties orig_vkGetPhysicalDeviceProperties = nullptr;
 
 void hooked_vkGetPhysicalDeviceProperties(VkPhysicalDevice physicalDevice, VkPhysicalDeviceProperties* pProperties) {
-    if (orig_vkGetPhysicalDeviceProperties) {
-        orig_vkGetPhysicalDeviceProperties(physicalDevice, pProperties);
-    }
+    if (orig_vkGetPhysicalDeviceProperties) orig_vkGetPhysicalDeviceProperties(physicalDevice, pProperties);
     pProperties->vendorID = 0x5143;
     pProperties->deviceID = 0x41383330;
     strcpy(pProperties->deviceName, "Adreno (TM) 830");
@@ -47,14 +39,9 @@ void hooked_vkGetPhysicalDeviceProperties(VkPhysicalDevice physicalDevice, VkPhy
 // =============================================================================
 void aktifkan_spoofing_driver() {
     void* gles_handle = dlopen("libGLESv2.so", RTLD_LAZY);
-    if (gles_handle) {
-        orig_glGetString = (PFNGLGETSTRINGPROC)dlsym(gles_handle, "glGetString");
-    }
-
+    if (gles_handle) orig_glGetString = (PFNGLGETSTRINGPROC)dlsym(gles_handle, "glGetString");
     void* vulkan_handle = dlopen("libvulkan.so", RTLD_LAZY);
-    if (vulkan_handle) {
-        orig_vkGetPhysicalDeviceProperties = (PFN_vkGetPhysicalDeviceProperties)dlsym(vulkan_handle, "vkGetPhysicalDeviceProperties");
-    }
+    if (vulkan_handle) orig_vkGetPhysicalDeviceProperties = (PFN_vkGetPhysicalDeviceProperties)dlsym(vulkan_handle, "vkGetPhysicalDeviceProperties");
 }
 
 // =============================================================================
@@ -62,32 +49,29 @@ void aktifkan_spoofing_driver() {
 // =============================================================================
 class GPUSpooferModule : public zygisk::ModuleBase {
 public:
-    void onLoad(zygisk::Api *api, JNIEnv *env) override {
-        this->api_peta = api;
-        this->env_peta = env;
+    void onLoad(zygisk::Api *api, JNIEnv *env) override { 
+        this->api_peta = api; 
+        this->env_peta = env; 
     }
-
+    
     void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
         const char *process_name = env_peta->GetStringUTFChars(args->nice_name, nullptr);
-
         if (!process_name) return;
-
-        // DAFTAR HITAM: Hindari aplikasi sistem agar tidak crash
-        if (strstr(process_name, "com.android.systemui") ||
+        
+        // Kode daftar hitam yang sudah diperbaiki total dari typo
+        if (strstr(process_name, "com.android.systemui") || 
+            strstr(process_name, "ksu.manager") || 
             strstr(process_name, "com.google.android") ||
-            strstr(process_name, "ksu.manager") ||
             strstr(process_name, "android.process")) {
-
+            
             api_peta->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
             env_peta->ReleaseStringUTFChars(args->nice_name, process_name);
             return;
         }
-
+        
         aktifkan_spoofing_driver();
-
         env_peta->ReleaseStringUTFChars(args->nice_name, process_name);
     }
-
 private:
     zygisk::Api *api_peta;
     JNIEnv *env_peta;
